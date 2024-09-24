c3e4c826f0f524829a3ddcb38bc4a1ed728d9c9b
kuanhuachen <kuanhua.chen@amazingtalker.com>
2024-09-24 15:55:17 +0800

    git add file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "InsiderMobile",
+  platforms: [.iOS(.v14)],
+  products: [
+    .library(
+      name: "TXLiteAVSDK",
+      targets: ["TXLiteAVSDK"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "TXLiteAVSDK",
+      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+    )
+  ]
+)
---
+++ README.md
@@ -0,0 +1,2 @@
+
+# TXListAVSDK for ios
