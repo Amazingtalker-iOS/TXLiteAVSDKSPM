658363e137754c6af671a4fc0fc6114c83079b3a
kuanhuachen <kuanhua.chen@amazingtalker.com>
2024-09-25 10:05:21 +0800

    fix package.swift file structure

--- Package.swift
+++ Package.swift
@@ -14,7 +14,22 @@ let package = Package(
   ],
   targets: [
     .binaryTarget(
-      name: "TXLiteAVSDK",
+      name: "TXLiteAVSDK_Live",
+      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+    ),
+    .binaryTarget(
+      name: "TXFFmpeg",
+      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+    ),
+    .binaryTarget(
+      name: "TXLiteAVSDK_ReplayKitExt",
+      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+    ),
+    .binaryTarget(
+      name: "TXSoundTouch",
       url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
       checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     )
