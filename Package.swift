54f4a4f65eba0c9dc084499bc0c6e67a3e9a7337
kuanhuachen <kuanhua.chen@amazingtalker.com>
2024-09-25 10:48:27 +0800

    test structure

--- Package.swift
+++ Package.swift
@@ -8,49 +8,49 @@ let package = Package(
   platforms: [.iOS(.v16)],
   products: [
     .library(
-      name: "TXLiteAVSDK_Live",
+      name: "TXLiteAVSDK",
       targets: [
-        "TXLiteAVSDK_Live"
-      ]
-    ),
-    .library(
-      name: "TXFFmpeg",
-      targets: [
-        "TXFFmpeg"
-      ]
-    ),
-    .library(
-      name: "TXLiteAVSDK_ReplayKitExt",
-      targets: [
-        "TXLiteAVSDK_ReplayKitExt"
-      ]
-    ),
-    .library(
-      name: "TXSoundTouch",
-      targets: [
-        "TXSoundTouch"
+        "TXLiteAVSDK_Live", "TXFFmpeg", "TXLiteAVSDK_ReplayKitExt", "TXSoundTouch"
       ]
     )
+//    .library(
+//      name: "TXFFmpeg",
+//      targets: [
+//        "TXFFmpeg"
+//      ]
+//    ),
+//    .library(
+//      name: "TXLiteAVSDK_ReplayKitExt",
+//      targets: [
+//        "TXLiteAVSDK_ReplayKitExt"
+//      ]
+//    ),
+//    .library(
+//      name: "TXSoundTouch",
+//      targets: [
+//        "TXSoundTouch"
+//      ]
+//    )
   ],
   targets: [
     .binaryTarget(
       name: "TXLiteAVSDK_Live",
-      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
-      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+      url: "https://drive.google.com/uc?export=download&id=1XSIygw0bBalYg16Q0Mhx4Xef6zud186T",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe",
     ),
     .binaryTarget(
       name: "TXFFmpeg",
-      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      url: "https://drive.google.com/uc?export=download&id=1XSIygw0bBalYg16Q0Mhx4Xef6zud186T",
       checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     ),
     .binaryTarget(
       name: "TXLiteAVSDK_ReplayKitExt",
-      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      url: "https://drive.google.com/uc?export=download&id=1XSIygw0bBalYg16Q0Mhx4Xef6zud186T",
       checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     ),
     .binaryTarget(
       name: "TXSoundTouch",
-      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      url: "https://drive.google.com/uc?export=download&id=1XSIygw0bBalYg16Q0Mhx4Xef6zud186T",
       checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     )
   ]
