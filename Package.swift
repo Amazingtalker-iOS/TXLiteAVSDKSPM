50cf65307a128e186015ed96a3e21612d732db41
kuanhuachen <kuanhua.chen@amazingtalker.com>
2024-09-25 11:55:34 +0800

    test s3 url

--- Package.swift
+++ Package.swift
@@ -35,24 +35,24 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "TXLiteAVSDK_Live",
-      url: "/Users/chenguanhua/Downloads/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     ),
     .binaryTarget(
       name: "TXFFmpeg",
-      url: "/Users/chenguanhua/Downloads/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     ),
     .binaryTarget(
       name: "TXLiteAVSDK_ReplayKitExt",
-      url: "/Users/chenguanhua/Downloads/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     ),
     .binaryTarget(
       name: "TXSoundTouch",
-      url: "/Users/chenguanhua/Downloads/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
-//      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
+//      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
+      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
     )
   ]
 )
