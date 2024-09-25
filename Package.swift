7b66e2348a1fd9abe2acf9acf3113b82207eba98
kuanhuachen <kuanhua.chen@amazingtalker.com>
2024-09-25 14:54:38 +0800

    fix seperate xcframeworks from one to four zip files

--- Package.swift
+++ Package.swift
@@ -13,47 +13,27 @@ let package = Package(
         "TXLiteAVSDK_Live", "TXFFmpeg", "TXLiteAVSDK_ReplayKitExt", "TXSoundTouch"
       ]
     )
-//    .library(
-//      name: "TXFFmpeg",
-//      targets: [
-//        "TXFFmpeg"
-//      ]
-//    ),
-//    .library(
-//      name: "TXLiteAVSDK_ReplayKitExt",
-//      targets: [
-//        "TXLiteAVSDK_ReplayKitExt"
-//      ]
-//    ),
-//    .library(
-//      name: "TXSoundTouch",
-//      targets: [
-//        "TXSoundTouch"
-//      ]
-//    )
   ],
   targets: [
     .binaryTarget(
       name: "TXLiteAVSDK_Live",
-      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/TXLiteAVSDK_Live.zip",
+      checksum: "6a73807872809b5564af972899d37b70bde1557694cccdb8728c5d069156fbba"
     ),
     .binaryTarget(
       name: "TXFFmpeg",
-      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/TXFFmpeg.zip",
+      checksum: "f7864061c916d686a8244898097f9388fa09ba3a39553d748a1317e944837fcb"
     ),
     .binaryTarget(
       name: "TXLiteAVSDK_ReplayKitExt",
-      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/TXLiteAVSDK_ReplayKitExt.zip",
+      checksum: "c2346b9b7689c78e9eeae77c0d55509b91c02e7347c0620f261ce77f1d639456"
     ),
     .binaryTarget(
       name: "TXSoundTouch",
-      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/LiteAVSDK_Live_iOS_test.zip",
-      checksum: "bc6fdb67b4b13f22cef7c3f88b59861ddf29b552ecaab73fe513a319504fde94"
-//      url: "https://liteav.sdk.qcloud.com/download/latest/TXLiteAVSDK_Live_iOS_latest.zip",
-//      checksum: "c057814c33d181626e776baae5a8ac0c3a45bc0f7c3b6a8d62c474c80063d7fe"
+      url: "https://at-staging-v2.s3.ap-northeast-1.amazonaws.com/yes/TXSoundTouch.zip",
+      checksum: "904297ff3770810b9e3c43db2b167b370196b0bc34e10ed0bd25681fb4dddb66"
     )
   ]
 )
